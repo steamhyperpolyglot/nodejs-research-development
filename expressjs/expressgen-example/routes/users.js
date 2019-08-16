048a4e8a605452d39fcc26b4254cc29beb041500
Sherman Chen <uxdailygeek@gmail.com>
2019-08-17 00:11:16 +0800

    Working on the Web API code example

--- expressjs/expressgen-example/routes/users.js
+++ expressjs/expressgen-example/routes/users.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/', (req, res) => {
+	return res.send('POST HTTP method on user resource');
+});
+
+router.put('/:userId', (req, res) => {
+	return res.send(`PUT HTTP method on users/${req.params.userId} resource`);
+});
+
+router.delete('/:userId', (req, res) => {
+	return res.send(`DELETE HTTP method on users/${req.params.userId} resource`);
+});
+
 module.exports = router;
