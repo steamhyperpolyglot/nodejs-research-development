5ba60fd836ce592ff656c5718f0d96e682c48357
Sherman Chen <uxdailygeek@gmail.com>
2019-08-17 00:01:55 +0800

    Also managed to add in the eslint support

--- expressjs/expressgen-example/app.js
+++ expressjs/expressgen-example/app.js
@@ -1,4 +1,5 @@
 let createError = require('http-errors');
+let cors = require('cors');
 let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
@@ -9,6 +10,8 @@ let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 
 let app = express();
+
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,18 +29,18 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === ' development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- expressjs/expressgen-example/routes/index.js
+++ expressjs/expressgen-example/routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+})
+
+router.post('/', (req, res) =>  {
+	return res.send('Received a POST HTTP method');
+});
+
+router.put('/', (req, res) =>  {
+	return res.send('Received a PUT HTTP method');
+});
+
+router.delete('/', (req, res) =>  {
+	return res.send('Received a DELETE HTTP method');
 });
 
 module.exports = router;
