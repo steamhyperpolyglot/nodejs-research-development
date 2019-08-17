8b2ec1f14d8ba8b36ae28d97b282a0a556d1ac61
Sherman Chen <uxdailygeek@gmail.com>
2019-08-17 21:41:41 +0800

    Working through the Web API example

--- expressjs/expressgen-example/routes/users.js
+++ expressjs/expressgen-example/routes/users.js
@@ -1,9 +1,38 @@
 let express = require('express');
 let router = express.Router();
+let uuidv4 = require('uuid/v4');
+
+let users = {
+	1: {
+		id: '1',
+		username: 'Sherman Chen',
+	},
+	2: {
+		id: '2',
+		username: 'Lucas Poh',
+	},
+};
+
+let messages = {
+	1: {
+		id: '1',
+		text: "Hello World",
+		userId: '1',
+	},
+	2: {
+		id: '2',
+		text: "By World",
+		userId: '2',
+	},
+};
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  return res.send(Object.values(users));
+});
+
+router.get('/users/:userId', (req, res) => {
+	return res.send(users[req.params.userId]);
 });
 
 router.post('/', (req, res) => {
@@ -16,6 +45,28 @@ router.put('/:userId', (req, res) => {
 
 router.delete('/:userId', (req, res) => {
 	return res.send(`DELETE HTTP method on users/${req.params.userId} resource`);
+});
+
+router.get('/messages', (req, res) => {
+	return res.send(Object.values(messages));
+});
+
+router.get('/messages/:messageId', (req, res) => {
+	return res.send(messages[req.params.messageId]);
+});
+
+router.post('/messages', (req, res) => {
+	//let messageText = req.body.text;
+
+	const id = uuidv4();
+	const message = {
+		id,
+		text: req.body.text
+	};
+	
+	messages[id] = message;
+	
+	return res.send(message);
 });
 
 module.exports = router;
